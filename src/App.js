c1e17fafdc7c65b4a770ba8b61298ac5f328d972
sellayuniar <synrr212@gmail.com>
2022-03-23 11:40:00 +0800

    memasukan file ke github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import data from "./single-sample"
+
+
+// const album = () => {
+  
+// };
+
+// const songTitle = () => {
+
+// }
+
+// const songArtist = () => {
+  
+// }
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      
+      <img src={data.album.images[1].url} alt="yuhu" />
+      <h1> {data.album.artists[0].name}</h1>
+      <h2>{data.album.name}</h2>
+    
+      <button>Select</button>
     </div>
+   
   );
 }
-
+  
 export default App;
